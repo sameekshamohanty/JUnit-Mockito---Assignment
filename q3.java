7b33714d46bc158d485e01069903be2d2d22f3b2
sameekshamohanty <78748985+sameekshamohanty@users.noreply.github.com>
2022-03-07 10:50:17 +0530

    Create q3.java

---
+++ q3.java
@@ -0,0 +1,24 @@
+package testing;
+
+public class q3 {
+    public static double withdraw(double amount) throws  InsufficientBalanceException {
+        double balance = 600 ;
+        try {
+            if(balance<amount){
+                throw new InsufficientBalanceException ("insufficient balance in the account");
+            }
+            balance -= amount ;
+            return balance;
+        }
+        catch (RuntimeException re){
+            System.out.println("exception");
+        }
+        return balance;
+    }
+}
+class InsufficientBalanceException extends Exception {
+    public InsufficientBalanceException(String str)
+    {
+        super(str);
+    }
+}
